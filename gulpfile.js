cec6f1479eb285fcfdc4357a555437b1135db2be
千葉桂一郎 <chibakeiichirou@MacBook-Air.local>
2023-03-17 12:44:42 +0900

    pug使用のためディレクトリ構成の修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // // gulpプラグインの読み込み
 const gulp = require("gulp");
+var pug = require("gulp-pug");
 
 // // Sassをコンパイルするプラグインの読み込み
 const gulpDartSass = require('gulp-dart-sass')
@@ -7,7 +8,7 @@ const sassGlob = require("gulp-sass-glob-use-forward");
 
 
 // style.scssをタスクを作成する
-gulp.task("default", () => {
+gulp.task("sass", () => {
   
   return gulp.src("sass/style.scss") // Sassのコンパイルを実行
     .pipe(sassGlob()) // Sassの@importにおけるglobを有効にする
@@ -15,4 +16,13 @@ gulp.task("default", () => {
       //     // cssフォルダー以下に保存
     .pipe(gulp.dest("css"));
     
+});
+
+gulp.task( 'pug', function() {
+	return gulp
+		.src([ './pug/**/*.pug', '!./pug/**/_*.pug' ])
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe( gulp.dest( './public' ) );
 });
